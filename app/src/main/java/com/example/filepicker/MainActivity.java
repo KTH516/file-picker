9ca4d13dd65d7b5c6b419c575dab30e2f6624a8d
Kelly Ho <Kth516@uw.edu>
2021-09-02 21:07:41 -0700

    Change minimum version

--- app/src/main/java/com/example/filepicker/MainActivity.java
+++ app/src/main/java/com/example/filepicker/MainActivity.java
@@ -3,21 +3,14 @@ package com.example.filepicker;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.ConfigurationInfo;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     // Initialize variable
